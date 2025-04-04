80ca3fe2a445b5098bf6021bba4980096e50a7b1
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-04 15:48:16 +0200

    LANCIO DADO completato, ora provo ad implementare una classifica.

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,7 +1,22 @@
-// -------- CONFIGURAZIONE
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
-// -------- INPUT
+alert("Lancio del dado del GIOCATORE 1");
 
-// -------- VALIDAZIONE INPUT
+const dadoPlayer1 = Math.floor(Math.random() * 6) + 1;
 
-// ----------------------------------------- ALGORITMO
+alert("Il tuo numero è:   " + dadoPlayer1);
+
+alert("Lancio del dado del COMPUTER");
+
+const dadoComputer = Math.floor(Math.random() * 6) + 1;
+
+alert("Il tuo numero è:   " + dadoComputer);
+
+if (dadoPlayer1 > dadoComputer) {
+  alert("PLAYER 1 VINCE");
+} else if (dadoPlayer1 < dadoComputer) {
+  alert("COMPUTER VINCE");
+} else {
+  alert("PAREGGIO");
+}
